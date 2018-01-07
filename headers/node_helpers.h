d09b09c560edee1e4f4105f683248803957cb433
David Khol <david.khol@ackee.cz>
2018-01-07 18:24:53 +0100

    Add missing header file.

---
+++ headers/node_helpers.h
@@ -0,0 +1,12 @@
+//
+// Created by DAVE on 31. 12. 2017.
+//
+
+#ifndef CRACKLE_INTERPRETER_NODE_HELPERS_H
+#define CRACKLE_INTERPRETER_NODE_HELPERS_H
+
+#include "types.h"
+
+dataTypeEnum widenNodes(constantNode* a, constantNode* b);
+
+#endif //CRACKLE_INTERPRETER_NODE_HELPERS_H
